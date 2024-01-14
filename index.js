9de78e97bb7bc009352a9201c0dae26abfbd7b6e
TakahiroT0415156 <teradatakahiro16@gmail.com>
2024-01-14 17:31:23 +0900

    デバックの追加

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ function shuffleDeck() {
 // 「カードを引く」ボタンが押された時に発火する関数
 document.querySelector("#ボタンのID").addEventListener("click", pullCard);
 function pullCard() {
+  if (isWin) {
+    // 自分がカードを引く
+    // 相手がカードを引く
+    // 画面を更新する
+  }
   // 自分がカードを1枚引く
   if (myCard.length <= 4) {
     // カードを1枚引く
@@ -57,16 +62,29 @@ function playGame() {
   // 自分と相手のカードの数字を見比べて勝負する
   // 勝敗を決める
   // 勝敗を画面に表示
+  // 勝敗フラグをtrueにする
 }
 
 // 「もう1度遊ぶ」ボタンが押された時に発火する関数
 document.querySelector("#ボタンのID").addEventListener("click", playAgain);
 function playAgain() {
-  // 画面を初期化する
+  // reloadメソッドを利用して画面を更新する
 }
 
 function loadScreen() {
   // 自分のカードを表示する
   // 相手のカードを表示する
   // カードの合計値を算出する
+}
+
+// デバック関数
+function debug() {
+  console.log("---------");
+  console.log(`カードの山は ${deck}`);
+  console.log("---------");
+  console.log(`自分のカードは ${myCard}`);
+  console.log("---------");
+  console.log(`相手のカードは ${partnersCard}`);
+  console.log("---------");
+  console.log(`勝敗フラグは ${finishFlag}`);
 }
